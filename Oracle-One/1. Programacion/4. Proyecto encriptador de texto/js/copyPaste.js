bfc69731aee257292d4a390b02c80bf30257388b
Jhonnyer Otalvaro <jorge.otalvaro00@usc.edu.co>
2022-08-10 01:25:52 -0500

    Proyecto encriptador de texto casi terminado

--- Oracle-One/1. Programacion/4. Proyecto encriptador de texto/js/copyPaste.js
+++
@@ -1,8 +0,0 @@
-//funcion para copiar el texto
- //funcion para copiar el texto ingresados
- function copiarTexto() {
-    let texto = document.getElementById('msg');
-    texto.select();
-    texto.setSelectionRange(0, 99999);
-    document.execCommand('copy');
-}
