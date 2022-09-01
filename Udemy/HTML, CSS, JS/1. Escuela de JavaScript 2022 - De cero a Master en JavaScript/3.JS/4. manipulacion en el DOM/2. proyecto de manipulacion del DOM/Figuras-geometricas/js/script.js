e252d1c8a20d07a8d59928bff493d8b138408c57
Jhonnyer Otalvaro <jorge.otalvaro00@usc.edu.co>
2022-09-01 14:36:46 -0500

    Agregando Patron modular & use strict
    El patron modular sirve para organizar mejor el codigo y que sea privado, el use strict ayuda a localizar mejor el error de sintaxis en js

--- Udemy/HTML, CSS, JS/1. Escuela de JavaScript 2022 - De cero a Master en JavaScript/3.JS/4. manipulacion en el DOM/2. proyecto de manipulacion del DOM/Figuras-geometricas/js/script.js
+++ Udemy/HTML, CSS, JS/1. Escuela de JavaScript 2022 - De cero a Master en JavaScript/3.JS/4. manipulacion en el DOM/2. proyecto de manipulacion del DOM/Figuras-geometricas/js/script.js
@@ -1,18 +1,31 @@
-//fucnion que calcula el area y perimetro del cuadrado
+//Agregando patron modular (Se realiza por medio de una function flecha): Lo cual sera codigo privado (para que nadie pueda acceder a las variables del programa)
+//no se podra visualisar desde consola
+//Se realiza de la siguiente manera...
+/**************************
+ *
+ (()=>){
+ Todo lo que va aqui a dentro queda privado
+ })();
+ */
 
-//usamos funcion flecha
-//funcion felcha para calcular el perimetro de un cuadrado
-const perimSquare= (lado) => lado * 4;
-//funcion flecha para encontrar el area de un cuadrado
-const area = (lado) => lado * lado;
-//Agregart una costante global del resultado 
-const valorResult = document.getElementById('result');
+(() => {
+  //El 'use strict' es un metodo que sirve para corregir errores de sintaxis
+  "use strict";
 
-//funcion para el boton perimeto 
-function calcularPerimetro(){
+  //fucnion que calcula el area y perimetro del cuadrado
 
+  //usamos funcion flecha
+  //funcion felcha para calcular el perimetro de un cuadrado
+  const perimSquare = (lado) => lado * 4;
+  //funcion flecha para encontrar el area de un cuadrado
+  const area = (lado) => lado * lado;
+  //Agregart una costante global del resultado
+  const valorResult = document.getElementById("result");
+
+  //funcion para el boton perimeto
+  function calcularPerimetro() {
     //Con esto toma el valor ingresado en el input de la pagina
-    let inputLado = document.getElementById('inputLado');
+    let inputLado = document.getElementById("inputLado");
     //Con esto hace la conversion del valor a entero
     let valor = Number(inputLado.value);
 
@@ -20,13 +33,11 @@ function calcularPerimetro(){
     const result = `El perimetro del cuadrado es: ${perimSquare(valor)} cm`;
     //document.getElementById('result').innerText = result;
     valorResult.innerText = result;
-}
+  }
 
-
-function calcularArea(){
-
+  function calcularArea() {
     //Con esto toma el valor ingresado en el input de la pagina
-    let inputLado = document.getElementById('inputLado');
+    let inputLado = document.getElementById("inputLado");
     //Con esto hace la conversion del valor a entero
     let valor = Number(inputLado.value);
 
@@ -34,75 +45,82 @@ function calcularArea(){
     const result = `El Area del cuadrado es: ${area(valor)} cm^2`;
     //document.getElementById('result').innerText = result;
     valorResult.innerText = result;
-}
+  }
 
-//funcion del perimetro de un triangulo 
-const perimTriangulo = (lado1 , lado2, lado3) => lado1 + lado2 + lado3;
+  //funcion del perimetro de un triangulo
+  const perimTriangulo = (lado1, lado2, lado3) => lado1 + lado2 + lado3;
 
-//funcion para encontrar el area de un triangulo
-const areaTriangulo = (base, altura) => base * altura / 2;
+  //funcion para encontrar el area de un triangulo
+  const areaTriangulo = (base, altura) => (base * altura) / 2;
 
-//funcion para el boton perimeto para encontrar el perimetro de un triangulo
-function calcularPerimTriangulo(){
+  //funcion para el boton perimeto para encontrar el perimetro de un triangulo
+  function calcularPerimTriangulo() {
     //con esto se toma el valor ingresado en el primer input de triangulo "lado"
-    let inputLado1 = document.getElementById('lado1T');
-    let inputLado2 = document.getElementById('lado2T');
-    let inputLado3 = document.getElementById('lado3T');
+    let inputLado1 = document.getElementById("lado1T");
+    let inputLado2 = document.getElementById("lado2T");
+    let inputLado3 = document.getElementById("lado3T");
     //Toma el valor ingresado y loTransformamos los valores ingresados a enteros para evitar error de sintaxis
     let valorL1 = Number(inputLado1.value);
     let valorL2 = Number(inputLado2.value);
     let valorL3 = Number(inputLado3.value);
 
     //sobre escritura del resultado en el DOM
-    const result = `El perimetro del triangulo es de: ${perimTriangulo(valorL1,valorL2,valorL3)} cm`;
+    const result = `El perimetro del triangulo es de: ${perimTriangulo(
+      valorL1,
+      valorL2,
+      valorL3
+    )} cm`;
     valorResult.innerText = result;
-}
+  }
 
-//funcion para el boton area para encontrar el area de un cuadrado
-function calcularAreaTriangulo(){
-    let inputBase = document.getElementById('base');
-    let inputAltura = document.getElementById('altura');
+  //funcion para el boton area para encontrar el area de un cuadrado
+  function calcularAreaTriangulo() {
+    let inputBase = document.getElementById("base");
+    let inputAltura = document.getElementById("altura");
 
     let baseT = Number(inputBase.value);
     let alturaT = Number(inputAltura.value);
 
-    const result = `El area del triangulor es de: ${areaTriangulo(baseT, alturaT)}`;
+    const result = `El area del triangulor es de: ${areaTriangulo(
+      baseT,
+      alturaT
+    )}`;
     valorResult.innerText = result;
-}
+  }
 
-//Solucion para el circulo
+  //Solucion para el circulo
 
-//funcion para encontrar el diametro de un cirtulo
-const diametroCirculo = (radio) => radio*2;
-//funcion para encontrar el perimetro de un Circulo
-const perimetroCirculo = (radio) => diametroCirculo(radio) * Math.PI;
-//funcion flecha para encontrar el area de un Circulo
-const areaCirculo = (radio) => (radio * radio) * Math.PI;
+  //funcion para encontrar el diametro de un cirtulo
+  const diametroCirculo = (radio) => radio * 2;
+  //funcion para encontrar el perimetro de un Circulo
+  const perimetroCirculo = (radio) => diametroCirculo(radio) * Math.PI;
+  //funcion flecha para encontrar el area de un Circulo
+  const areaCirculo = (radio) => radio * radio * Math.PI;
 
-
-//funcion para boton Diametro para encontrar el Diametro del circulo
-function calcularDiametroCirculo(){
-    let inputRadio = document.getElementById('radioC');
+  //funcion para boton Diametro para encontrar el Diametro del circulo
+  function calcularDiametroCirculo() {
+    let inputRadio = document.getElementById("radioC");
     let radio = Number(inputRadio.value);
 
     const result = `El diametro del circulo es: ${diametroCirculo(radio)}`;
     valorResult.innerText = result;
-}
+  }
 
-//funcion para boton Perimetro para encontrar el Perimetro del circulo
-function calcularPerimetroCirculo(){
-    let inputRadio = document.getElementById('radioC');
+  //funcion para boton Perimetro para encontrar el Perimetro del circulo
+  function calcularPerimetroCirculo() {
+    let inputRadio = document.getElementById("radioC");
     let radio = Number(inputRadio.value);
 
     const result = `El Perimetro del circulo es: ${perimetroCirculo(radio)}`;
     valorResult.innerText = result;
-}
+  }
 
-//funcion para boton Area para encontrar el Area del circulo
-function calcularAreaCirculo(){
-    let inputRadio = document.getElementById('radioC');
+  //funcion para boton Area para encontrar el Area del circulo
+  function calcularAreaCirculo() {
+    let inputRadio = document.getElementById("radioC");
     let radio = Number(inputRadio.value);
 
     const result = `El Area del circulo es: ${areaCirculo(radio)}`;
     valorResult.innerText = result;
-}
+  }
+})();
