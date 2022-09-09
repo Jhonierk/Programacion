1271347326e9346ad127d81ed9c1434de7aed073
Jhonnyer Otalvaro <jorge.otalvaro00@usc.edu.co>
2022-09-09 16:44:18 -0500

    intefaz de login

--- CursoPhp/youtube/CRUD/admin/index.php
+++ CursoPhp/youtube/CRUD/admin/index.php
@@ -1,14 +1,49 @@
-<!DOCTYPE html>
+<!doctype html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>administrador del sitio web</title>
-</head>
-<body>
-    
-    Login
+  <head>
+    <title>Title</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-</body>
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+  </head>
+  <body>
+      
+    <div class="container">
+        <div class="row">
+        <div class="col-md-3"></div>
+            <div class="col-md-5">
+            <br><br><br><br>
+            <div class="card">
+                <div class="card-header">
+                    Login
+                </div>
+                <div class="card-body">
+                    <form>
+                    <div class = "form-group">
+                    <label for="exampleInputEmail1">Usuario</label>
+                    <input type="text" class="form-control" name="usuario"  placeholder="Ingrese su usuario">
+                    
+                    </div>
+                    <div class="form-group">
+                    <label for="exampleInputPassword1">Contraseña</label>
+                    <input type="password" class="form-control" name="contrasenia" placeholder="Ingrese su contraseña">
+                    </div>
+                    
+                    <button type="submit" class="btn btn-primary">Entrar</button>
+                    </form>
+                    
+                    
+                </div>
+                
+            </div>
+                
+            </div>
+            
+        </div>
+    </div>  
+
+  </body>
 </html>
