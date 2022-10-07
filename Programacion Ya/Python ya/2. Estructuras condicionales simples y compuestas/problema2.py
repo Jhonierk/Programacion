71574430932021b00ff73579dee1399458bb3bf4
Jhonnyer Otalvaro <jorge.otalvaro00@usc.edu.co>
2022-10-07 17:09:22 -0500

    Cual es el numero mayor?
    Realizar un programa que solicite ingresar dos números distintos y muestre por pantalla el mayor de ellos.

---
+++ Programacion Ya/Python ya/2. Estructuras condicionales simples y compuestas/problema2.py
@@ -0,0 +1,11 @@
+"""
+Realizar un programa que solicite ingresar dos números distintos y muestre por pantalla el mayor de ellos.
+"""
+
+num1=int(input("Ingrese primer valor:"))
+num2=int(input("ingrese segundo valor:"))
+print("El valor mayor es")
+if num1>num2:
+    print(num1)
+else:
+    print(num2)
