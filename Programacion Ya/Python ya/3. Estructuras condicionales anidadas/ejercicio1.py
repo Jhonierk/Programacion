f8bd5de4be9e442bbef3fc0228f9a6f7eaafb079
Jhonnyer Otalvaro <jorge.otalvaro00@usc.edu.co>
2022-10-07 17:28:04 -0500

    entre 3 quien es mayor?
    Se cargan por teclado tres números distintos. Mostrar por pantalla el mayor de ellos.

---
+++ Programacion Ya/Python ya/3. Estructuras condicionales anidadas/ejercicio1.py
@@ -0,0 +1,16 @@
+"""
+Se cargan por teclado tres números distintos. Mostrar por pantalla el mayor de ellos.
+"""
+num1=int(input("Ingrese primer valor:"))
+num2=int(input("Ingrese segunda valor:"))
+num3=int(input("Ingrese tercer valor:"))
+if num1>num2:
+    if num1>num3:
+        print(num1)
+    else:
+        print(num3)
+else:
+    if num2>num3:
+        print(num2)
+    else:
+        print(num3)
