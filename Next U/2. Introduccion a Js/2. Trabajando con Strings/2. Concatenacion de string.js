b017ace5b8f5ade016361fc1c77a24a85a20982d
Jhonnyer Otalvaro <jorge.otalvaro00@usc.edu.co>
2022-10-07 16:41:59 -0500

    Algunos cambios Para ordenar

--- Next U/2. Introduccion a Js/2. Trabajando con Strings/2. Concatenacion de string.js
+++
@@ -1,6 +0,0 @@
-function saludar(nombre, apellido, curso){
-    let resultado = `Hola ${nombre} ${apellido}, bienvenido al curso ${curso}`;
-    return resultado;
-}
-
-console.log(saludar("Kira", "Negra", "C++"));
