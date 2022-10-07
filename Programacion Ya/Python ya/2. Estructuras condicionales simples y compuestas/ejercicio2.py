f981342830db12133a23c3d9e7d996ed2e1460f7
Jhonnyer Otalvaro <jorge.otalvaro00@usc.edu.co>
2022-10-07 17:17:57 -0500

    Promedio de notas con if
    Se ingresan tres notas de un alumno, si el promedio es mayor o igual a siete mostrar un mensaje "Promocionado".

--- Programacion Ya/Python ya/2. Estructuras condicionales simples y compuestas/ejercicio2.py
+++ Programacion Ya/Python ya/2. Estructuras condicionales simples y compuestas/ejercicio2.py
@@ -1,5 +1,5 @@
 """
-Realizar un programa que solicite la carga por teclado de dos números, si el primero es mayor al segundo informar su suma y diferencia, en caso contrario informar el producto y la división del primero respecto al segundo.
+Se ingresan tres notas de un alumno, si el promedio es mayor o igual a siete mostrar un mensaje "Promocionado".
 """
 nota1=int(input("Por favor ingrese nota1  :"))
 nota2=int(input("Por favor ingrese nota2  :"))
