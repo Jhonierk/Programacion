52e6ca660b208a02c9a2fbc28653a7418bb952c1
Jhonnyer Otalvaro <jorge.otalvaro00@usc.edu.co>
2022-10-09 14:23:23 -0500

    Valida si la cadena(email) ingresado tiene 1 o mas de un @
    Ingresar un mail por teclado. Verificar si el string ingresado contiene solo un caracter "@".

---
+++ Programacion Ya/Python ya/7. Procesar cadenas de caracteres/problema3.py
@@ -0,0 +1,17 @@
+"""
+Ingresar un mail por teclado. Verificar si el string ingresado contiene solo un caracter "@".
+"""
+
+mail=input("Ingrese un mail:")
+cantidad=0
+x=0
+while x<len(mail):
+    if mail[x]=="@":
+        cantidad=cantidad+1
+    x=x+1
+if cantidad==1:
+    print("Contiene solo un caracter @ el mail ingresado")
+if cantidad > 1:
+    print("El valor ingresado contiene mas de un @")
+else:
+    print("Incorrecto")
