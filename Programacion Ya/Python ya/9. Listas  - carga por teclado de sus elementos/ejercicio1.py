2cde8ff77248fc27baa40a3b9819095e37fec61f
Jhonnyer Otalvaro <jorge.otalvaro00@usc.edu.co>
2022-10-10 14:33:53 -0500

    Correccion del ejercicio

--- Programacion Ya/Python ya/9. Listas  - carga por teclado de sus elementos/ejercicio1.py
+++ Programacion Ya/Python ya/9. Listas  - carga por teclado de sus elementos/ejercicio1.py
@@ -2,13 +2,16 @@
 Almacenar en una lista los sueldos (valores float) de 5 operarios. Imprimir la lista y el promedio de sueldos.
 """
 
-lista=[]
-cantList = 5
-sumaSueldo = 0
-for x in range(cantList):
-    sueldos = float(input("Por favor ingrese el sueldo del empleado: "+str(x)+ " : "))
-    sumaSueldo += sueldos
-    promedio = sumaSueldo / cantList
 
-print("La suma de todos los sueldos es: "+str(sumaSueldo))
-print("Y el promedio del sueldo es de: "+str(promedio))
+sueldos=[]
+suma=0
+for x in range(5):
+    valor=float(input("Ingrese el sueldo del operario:"))
+    sueldos.append(valor)
+    suma=suma+valor
+
+print("Lista de sueldos")
+print(sueldos)
+promedio=suma/5
+print("Promedio de sueldos")
+print(promedio)
